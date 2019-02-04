3364d0981fcd082b8ca980a3e64fdf10d28ba63e
Ysabelle Lam <38703390+ysabellelam@users.noreply.github.com>
2019-02-04 00:06:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var	express		= require('express'),
-	app			=express();
+var	express = require('express'),
+  	bodyParser = require('body-parser'),
+	app = express();
 
-app.set('port',(process.env.PORT || 3000));
+var indexRoutes = require('./routes/index'),
+	apiRoutes = require('./routes/api');
+
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-	res.render('home');
-});
+/**** ROUTES ****/
+app.use('/', indexRoutes);
+app.use('/api', apiRoutes);
 
-app.get('/details', (req, res) => {
-	res.render('details');
-})
-
-app.listen(app.get('port'), () => console.log('Listening on port' + app.get('port')));
+app.listen(app.get('port'), () => console.log('Listening on port ' + app.get('port')));
